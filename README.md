4593e2e042fcd6c8d1bb4ee1f0b8871f303343a2
micangl <142919381+micangl@users.noreply.github.com>
2023-10-13 20:46:44 +0200

    Finish implementing bibtex parser.

--- README.md
+++ README.md
@@ -6,11 +6,14 @@ Based on [@hrsh7th](https://github.com/hrsh7th)'s [cmp-omni](https://github.com/
 Provides support for:
 - Fuzzy matching against all info provided by Vimtex (including bibliographic details, useful for citations):
   ![](https://github.com/micangl/cmp-vimtex/assets/142919381/4887b19b-d08d-44e3-9b29-22e91a3a1728)
+- Parse and display all details contained in bibtex files:
+  ![](https://github-production-user-asset-6210df.s3.amazonaws.com/142919381/274990752-d9cba239-aa54-4398-a17f-02f6eec1d628.png)
+- Trimming long strings in the completion menu (adds space to for the documenation window):
+  ![](https://github.com/micangl/cmp-vimtex/assets/142919381/bed1ab56-09cf-486c-baa9-be4198e52ce0)
 - Conveniently toggling symbols and additional information shown in the completion menu:
   ![](https://github.com/micangl/cmp-vimtex/assets/142919381/fc167389-134d-4a7c-b083-2c9eafe98891)
   ![](https://github.com/micangl/cmp-vimtex/assets/142919381/daa3c5b3-b3a7-46d4-a3e6-427b9d4371de)
-- Displaying additional information provided by Vimtex in a separate documentation window:
-  ![](https://github.com/micangl/cmp-vimtex/assets/142919381/eb3d8605-037c-4d0e-bd59-6144d428db1b)
+- Granuarly configuring the menus, and more (check out the sections below).
 
 # Installation
 
@@ -43,50 +46,67 @@ require("lazy").setup({
 # Setup
 
 ```lua
-require'cmp'.setup {
+require('cmp').setup({
   sources = {
-    {
-      name = 'vimtex',
-      option = {
-        --Set eventual options. For example,
-        --info_in_menu = 1,
-        --match_against_info = 1,
-      },
-    },
+    { name = 'vimtex', },
   },
-}
+})
+
+require('cmp_vimtex').setup({
+    -- Eventual options can be specified here.
+    -- See below for further details.
+})
 ```
 
 # Options
 
-### info_in_markdown: integer
-default: 1
+These are the default values of the configuration options:
 
-Use markdown in documentation window, to improve readability for additional bibliographic information.
+```
+require('cmp_vimtex').setup({
+    additional_information = {
+        info_in_menu = 1,
+        info_in_window = 1,
+        info_max_length = 60,
+        match_against_info = 1,
+        symbols_in_menu = 1,
+    },
+    bibtex_parser = {
+        enabled = 1,
+    },
+})
+```
 
-### info_in_menu: integer
+### additional_information.info_in_menu: integer
 default: 1
 
 Show detailed information (such as citations details) in the completion menu.
 
-### info_in_window: integer
+### additional_information.info_in_window: integer
 default: 1
 
 Show detailed information (such as citations details) in the documentation window.
 
-### info_max_length: integer
+### additional_information.info_max_length: integer
 default: 60
 
 Limit length (width) of additional info shown in the completion menu to the specified number of characters.
 To turn off this feature, set the option to a negative value.
 
-### match_against_info: integer
+### additional_information.match_against_info: integer
 default: 1
 
 Fuzzy match against both keyword and description.
 Particularly useful when completing citations, since the user can simply type the author/title/publication date.
 
-### symbols_in_menu: integer
+### additional_information.symbols_in_menu: integer
 default: 1
 
 Show sybmols associated with Latex keywords inside completion menu.
+
+### bibtex_parser.enabled: integer
+default: 1
+
+The source comes with a bibtex parser (a lua port of Vimtex's own), used to display, inside the documentation window, all of the bibliographic informations contained in the files.
+
+If the parser is disabled, the plugin will only show author/title/publication date (provided directly by Vimtex's omnifunc).
--- lua/cmp_vimtex/init.lua
+++ lua/cmp_vimtex/init.lua
@@ -1,162 +1,19 @@
-local source = {}
-local uv = vim.loop.version()
+local M = {}
 
-local defaults = {
-    info_in_markdown = 1,
-    info_in_menu = 1,
-    info_in_window = 1,
-    info_max_length = 60,
-    match_against_info = 1,
-    parse_bibtex = 1,
-    symbols_in_menu = 1,
-}
-
-source.newbib_r = function(self)
-    local parser = require('cmp_vimtex.parser')
-
-    vim.cmd([[call vimtex#paths#pushd(b:vimtex.root)]])
-    local files = vim.fn['vimtex#bib#files']()
-    for _, file in pairs(files) do
-        if self.bib_files[file] == nil then
-            local new_parser = parser.new(file)
-            new_parser:start_parsing()
-            self.bib_files[file] = new_parser
+local group = vim.api.nvim_create_augroup('cmp_vimtex', {clear = true})
+vim.api.nvim_create_autocmd({"BufWinEnter",}, {
+    pattern = '*.tex',
+    group = group,
+    callback = function()
+        if cmp_vimtex_global.config.bibtex_parser.enabled == 1 then
+            cmp_vimtex_global.start_parser(cmp_vimtex_global)
         end
-    end
+    end,
+})
+
+
+M.setup = function(options)
+    require('cmp').register_source('vimtex', require('cmp_vimtex.source').new(options))
 end
 
-local apply_config = function(user_config)
-    return vim.tbl_deep_extend("keep", user_config or {}, defaults)
-end
-
-source.new = function(options)
-  local self = setmetatable({}, { __index = source })
-  self.bib_files = {}
-  self.config = apply_config(options)
-  cmp_vimtex_global = self
-
-  return self
-end
-
-source.is_available = function()
-  return vim.bo.omnifunc ~= '' and vim.api.nvim_get_mode().mode == 'i'
-end
-
-source.get_position_encoding_kind = function()
-  return 'utf-8'
-end
-
-source.get_keyword_pattern = function()
-  return [[\k\+]]
-end
-
-source.get_trigger_characters = function()
-    return { '{' }
-end
-
-source.complete = function(self, params, callback)
-  local config = self.config
-
-  local offset_0 = self:_invoke(vim.bo.omnifunc, { 1, '' })
-  if type(offset_0) ~= 'number' then
-    return callback()
-  end
-  local result = self:_invoke(vim.bo.omnifunc, { 0, string.sub(params.context.cursor_before_line, offset_0 + 1) })
-  if type(result) ~= 'table' then
-    return callback()
-  end
-
-  local text_edit_range = {
-    start = {
-      line = params.context.cursor.line,
-      character = offset_0,
-    },
-    ['end'] = {
-      line = params.context.cursor.line,
-      character = params.context.cursor.character,
-    },
-  }
-
-  local items = {}
-  for _, v in ipairs(result) do
-    local menuLength = (v.menu ~= nil and string.len(v.menu) or 3)
-
-    if type(v) == 'string' then
-      table.insert(items, {
-        label = v,
-        textEdit = {
-          range = text_edit_range,
-          newText = v,
-        },
-      })
-    elseif type(v) == 'table' then
-
-      local _item = {
-        label = v.abbr or v.word,
-        textEdit = {
-          range = text_edit_range,
-          newText = v.word,
-        },
-        labelDetails = {
-          detail = v.kind,
-        },
-      }
-
-      if config.info_in_menu == 1 and menuLength > 3 then
-        _item.labelDetails.description = v.menu
-
-        -- Inspired by https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-1844480
-        if config.info_max_length >= 0 and v.menu:len() > config.info_max_length then
-          _item.labelDetails.description = vim.fn.strcharpart(_item.labelDetails.description, 0, config.info_max_length) .. '…'
-        end
-      end
-      
-      if config.info_in_window == 1 and v.info ~= nil then
-        if config.parse_bibtex == 1 then
-          for _, data in pairs(self.bib_files) do
-            if data.indexed == 1  and data.result[_item.label] ~= nil then
-              _item.documentation = {
-                kind = 'markdown',
-                value = data.result[_item.label].info,
-              }
-            end
-          end
-        else
-          _item.documentation = {
-            kind = 'markdown',
-            -- "%u%u+" specifies at least two consecutive uppercase letters.
-            value = string.gsub(v.info, "%u%u+", function(s) return "**" .. s .. "**" end),
-          }
-        end
-      end
-
-      if config.match_against_info == 1 then
-        _item.filterText = (v.abbr or v.word) .. (v.menu ~= nil and (" " .. v.menu) or "")
-      end
-
-      -- Symbols should have a length of 1 but, since most of them are Unicode
-      -- characters, are more than 1 byte long (up to 3). Unfortunately, Lua
-      -- counts using the number of bytes, since it doesn't support Unicode.
-      if config.symbols_in_menu == 1 and menuLength <= 3 then
-        _item.labelDetails.description = v.menu
-      end
-
-      table.insert(items, _item)
-    end
-  end
-  callback({ items = items })
-end
-
-source._invoke = function(_, func, args)
-  local prev_pos = vim.api.nvim_win_get_cursor(0)
-  local _, result = pcall(function()
-    return vim.fn['cmp_vimtex#invoke'](func, args)
-  end)
-  local next_pos = vim.api.nvim_win_get_cursor(0)
-  if prev_pos[1] ~= next_pos[1] or prev_pos[2] ~= next_pos[2] then
-    vim.api.nvim_win_set_cursor(0, prev_pos)
-  end
-  return result
-end
-
-return source
+return M
